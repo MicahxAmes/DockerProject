654d9f84f5b9f810d3a0ec2b4ef4bd42aea1c2fe
MicahxAmes <amesmicah@gmail.com>
2024-08-01 21:05:48 -0400

    Additional resources added

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! This is a simple Node.js app running on a Docker container.');
 });
 
 app.listen(port, () => {
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,23 @@
 provider "aws" {    
-  region = "us-west-1"  
+  region = "us-east-1"  
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet" {
+resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
 
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.main.id
@@ -33,27 +41,53 @@ resource "aws_ecs_cluster" "main" {
   name = "my-cluster"
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "sts:AssumeRole",
+        Effect: "Allow",
+        Principal: {
+          Service: "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "main" {
   family                   = "my-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "my-app"
-      image     = "YOUR_ECR_REPOSITORY_URL:latest"
-      essential = true
+      name      = "myapp-webserver"
+      image     = "my-image",
+      essential = true,
+      memory    = 512,
+      cpu       = 256,
       portMappings = [
         {
-          containerPort = 3000
-          hostPort      = 3000
+          containerPort = 80
+          hostPort      = 80
         }
       ]
     }
   ])
 }
+
 
 resource "aws_ecs_service" "main" {
   name            = "my-service"
@@ -63,13 +97,13 @@ resource "aws_ecs_service" "main" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.subnet.id]
+    subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
     security_groups = [aws_security_group.sg.id]
   }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.main.arn
-    container_name   = "my-app"
+    container_name   = "myapp-webserver"
     container_port   = 3000
   }
 }
@@ -79,7 +113,7 @@ resource "aws_lb" "main" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg.id]
-  subnets            = [aws_subnet.subnet.id]
+  subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
 }
 
 resource "aws_lb_target_group" "main" {
